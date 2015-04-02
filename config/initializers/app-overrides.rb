005f12982a776c86a2f3165518895a2e4b67d8e1
Steven Bull <steven@thebulls.us>
2015-04-01 19:10:16 -0700

    Use rails_12factor in development instead of setting Rails.logger.

--- config/initializers/app-overrides.rb
+++ config/initializers/app-overrides.rb
@@ -1,5 +1,6 @@
 if Rails.env.development?
-  Rails.logger = Logger.new(STDOUT) # Get logs to spit out when using foreman/puma.
+  # Rails.logger = Logger.new(STDOUT)
+  # To get logs to spit out when using foreman/puma, use the rails_12factor gem in development.
   Rails.application.config.assets.debug = false
   # Enable concurrent requests.
   # http://stackoverflow.com/questions/21605318/how-can-i-serve-requests-concurrently-with-rails-4
