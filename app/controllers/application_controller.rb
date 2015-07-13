5b45bafbbe8cc243fcf091a3b1a35c1c4a7faab1
Steven Bull <steven@thebulls.us>
2015-07-13 16:06:03 -0700

    Updated RailsAdmin param cleaning.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,25 +12,30 @@ class ApplicationController < ActionController::Base
   private
 
   def clean_params!
-    clean_params_helper(params) if params.present? && !skip_clean_params?
+    clean_params_helper(params, skip_clean_params?) if params.present?
   end
-  def clean_params_helper(params)
+  def clean_params_helper(params, skip_level=nil)
     # 2013-06-25: In Rails' multiparameter attribute assignment code,
     # .empty? is called on the attribute value.
     # nil doesn't have #empty?, so avoid multiparameter values.
     params.each do |k,v|
-      if v.blank? && !k.to_s.include?('(')
-        params[k] = nil
-      elsif v.is_a?(String)
-        params[k] = v.strip
-      elsif v.is_a?(Hash)
+      if v.is_a?(Hash)
         # Crawl the branches.
         clean_params_helper(v)
+      elsif !skip_level
+        # 2015-07-13: RailsAdmin actions often depend on existence
+        # of top-level params ("_continue" for cancel buttons,
+        # "csv" for export, ...).
+        if v.blank? && !k.to_s.include?('(')
+          params[k] = nil
+        elsif v.is_a?(String)
+          params[k] = v.strip
+        end
       end
     end
   end
   def skip_clean_params?
-    self.class.name == 'RailsAdmin::MainController' && action_name == 'export'
+    self.class.name == 'RailsAdmin::MainController'
   end
 
 end
